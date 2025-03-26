352ca9c78473be4d2d2e1bf51fd62815df80b6a4
Vu Duy Le <vuduyle004@gmail.com>
2025-03-27 00:55:00 +0700

    test

--- src/main/java/hsf302/se1868vj/he187349/RestaurantApplication.java
+++ src/main/java/hsf302/se1868vj/he187349/RestaurantApplication.java
@@ -18,6 +18,8 @@ public class RestaurantApplication extends Application {
     }
 
     public static void main(String[] args) {
+
+        System.out.println("test Jenkins");
         launch();
     }
 }
